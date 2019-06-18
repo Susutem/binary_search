a700398fcd7f7b9039d212680779bebfbe7359c5
Susuta Ryosuke <susut_t1999@icloud.com>
2019-06-18 17:25:58 +0900

    comment

--- src/apple.c
+++ src/apple.c
@@ -5,13 +5,25 @@ int k;
 int A[100000];
 
 
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 100000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+while (ub - lb > 1) {
+    int m = (lb + ub) /2;
+    int i, sigma = 0;
+    for (i = 0; i < n; i++) {
+        sigma = sigma + (A[i] + m -1) /m;
+  }
+    if (sigma <= k) ub = m;
+    else lb = m;
+}
+printf("%d\n", ub);
   return 0;
 }
