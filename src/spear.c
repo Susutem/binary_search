8afc91b916477b456b76fca9e5701ba4cb004766
Susuta Ryosuke <susut_t1999@icloud.com>
2019-06-18 17:41:10 +0900

    comment

--- src/spear.c
+++ src/spear.c
@@ -8,9 +8,22 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 100000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  while (ub - lb > 1) {
+    int m = (lb + ub) /2;
+    int i, sigma = 0;
+    for (i = 0; i < n; i++) {
+        sigma = sigma + A[i]/m;
+  }
+    if (sigma >= k) lb = m;
+    else ub = m;
+}
+printf("%d\n", lb);
+  
 
 
   return 0;
