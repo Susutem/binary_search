cf23058f3a1948676ed93e413428d9636b794e48
Susuta Ryosuke <susut_t1999@icloud.com>
2019-06-18 16:38:57 +0900

    comment

--- src/array.c
+++ src/array.c
@@ -8,11 +8,19 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = -1;
+  ub = n;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  if (A[n-1] < k)  printf("%d\n", n);
+  else {
+    while(ub - lb > 1) {
+        int m = (lb + ub) / 2;
+        if (A[m] >= k) ub = m;
+        else lb = m;
+    }
+    printf("%d\n", ub);
+}
   return 0;
 }
