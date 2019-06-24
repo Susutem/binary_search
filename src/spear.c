92f9d0fb7993d475b0e69a3b8301a29944023b57
Susuta Ryosuke <susut_t1999@icloud.com>
2019-06-24 16:55:34 +0900

    comment

--- src/spear.c
+++ src/spear.c
@@ -8,11 +8,18 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  lb = 0;
-  ub = 100000;
+  
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  
+  int max = 0;
+  for (i = 0; i < n; i++){
+    if (A[i] > max) max = A[i];
+  }
+  lb = 0;
+  ub = max;
+  
   while (ub - lb > 1) {
     int m = (lb + ub) /2;
     int i, sigma = 0;
