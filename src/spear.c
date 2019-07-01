191907c31266d00b761eee1681da624850c955d6
Susutem <susut_t1999@icloud.com>
2019-07-01 17:57:50 +0900

    spear

--- src/spear.c
+++ src/spear.c
@@ -8,18 +8,18 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  
+
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  
+
   int max = 0;
   for (i = 0; i < n; i++){
     if (A[i] > max) max = A[i];
   }
   lb = 0;
-  ub = max;
-  
+  ub = max+1;
+
   while (ub - lb > 1) {
     int m = (lb + ub) /2;
     int i, sigma = 0;
@@ -30,7 +30,7 @@ int main(){
     else ub = m;
 }
 printf("%d\n", lb);
-  
+
 
 
   return 0;
